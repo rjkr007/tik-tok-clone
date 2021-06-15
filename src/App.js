089e2ed1537e4d36ccfa5467bb2ad3a8ed00115c
RAJEEV KUMAR <rjkr.au@gmail.com>
2021-06-15 16:29:58 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Video from "./Video";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //BEM
+    <div className="app">
+      {/* <h1>Hello, Let's build Tik Tok Clone</h1> */}
+
+      <div className="app__videos">
+        <Video
+          url="https://webapp-va.tiktok.com/4771aee202bc8212b6c55463f02329ea/60c89343/tos-useast2a-ve-0068c002/bf347dafb09a467e9d6bdc9ea3532bd8?a=1988&br=3000&bt=1500&cd=0%7C0%7C0&ch=0&cr=0&cs=0&dr=3&ds=3&er=&l=20210615054511010190209081210B52F8&lr=tiktok&mime_type=video_mp4&net=0&pl=0&policy=eyJ2bSI6MiwiY2siOiJ0dF93ZWJpZF92MiIsInVpZCI6IjY5NzM3MzIyMjc5MTQ3Mjg0NDkifQ%3D%3D&qs=0&rc=M3Z4ODo7Ojk2NjMzNzgzM0ApaTVkOzVmNzxoNzw2Nmk4M2drbWUuZ2QzbjFgLS1kLzZzc2MuNDNeLWAzYjUwLmBeYDQ6Yw%3D%3D&vl=&vr="
+          channel="rk"
+          description="Wow it works"
+          song="khushi .."
+          likes={100}
+          messages={300}
+          shares={200}
+        />
+        <Video
+          url="https://webapp-va.tiktok.com/4771aee202bc8212b6c55463f02329ea/60c89343/tos-useast2a-ve-0068c002/bf347dafb09a467e9d6bdc9ea3532bd8?a=1988&br=3000&bt=1500&cd=0%7C0%7C0&ch=0&cr=0&cs=0&dr=3&ds=3&er=&l=20210615054511010190209081210B52F8&lr=tiktok&mime_type=video_mp4&net=0&pl=0&policy=eyJ2bSI6MiwiY2siOiJ0dF93ZWJpZF92MiIsInVpZCI6IjY5NzM3MzIyMjc5MTQ3Mjg0NDkifQ%3D%3D&qs=0&rc=M3Z4ODo7Ojk2NjMzNzgzM0ApaTVkOzVmNzxoNzw2Nmk4M2drbWUuZ2QzbjFgLS1kLzZzc2MuNDNeLWAzYjUwLmBeYDQ6Yw%3D%3D&vl=&vr="
+          channel="rk"
+          description="Wow it works"
+          song="khushi khushi.."
+          likes={123}
+          messages={400}
+          shares={200}
+        />
+        <Video
+          url="https://webapp-va.tiktok.com/4771aee202bc8212b6c55463f02329ea/60c89343/tos-useast2a-ve-0068c002/bf347dafb09a467e9d6bdc9ea3532bd8?a=1988&br=3000&bt=1500&cd=0%7C0%7C0&ch=0&cr=0&cs=0&dr=3&ds=3&er=&l=20210615054511010190209081210B52F8&lr=tiktok&mime_type=video_mp4&net=0&pl=0&policy=eyJ2bSI6MiwiY2siOiJ0dF93ZWJpZF92MiIsInVpZCI6IjY5NzM3MzIyMjc5MTQ3Mjg0NDkifQ%3D%3D&qs=0&rc=M3Z4ODo7Ojk2NjMzNzgzM0ApaTVkOzVmNzxoNzw2Nmk4M2drbWUuZ2QzbjFgLS1kLzZzc2MuNDNeLWAzYjUwLmBeYDQ6Yw%3D%3D&vl=&vr="
+          channel="rk"
+          description="Wow it works"
+          song="khushi khushi khushi..."
+          likes={153}
+          messages={200}
+          shares={205}
+        />
+        <Video
+          url="https://webapp-va.tiktok.com/4771aee202bc8212b6c55463f02329ea/60c89343/tos-useast2a-ve-0068c002/bf347dafb09a467e9d6bdc9ea3532bd8?a=1988&br=3000&bt=1500&cd=0%7C0%7C0&ch=0&cr=0&cs=0&dr=3&ds=3&er=&l=20210615054511010190209081210B52F8&lr=tiktok&mime_type=video_mp4&net=0&pl=0&policy=eyJ2bSI6MiwiY2siOiJ0dF93ZWJpZF92MiIsInVpZCI6IjY5NzM3MzIyMjc5MTQ3Mjg0NDkifQ%3D%3D&qs=0&rc=M3Z4ODo7Ojk2NjMzNzgzM0ApaTVkOzVmNzxoNzw2Nmk4M2drbWUuZ2QzbjFgLS1kLzZzc2MuNDNeLWAzYjUwLmBeYDQ6Yw%3D%3D&vl=&vr="
+          channel="rk"
+          description="Wow it works"
+          song="khushi............."
+          likes={183}
+          messages={300}
+          shares={290}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkSSuU_9raS-W1gA419ElhvXMvOr1j2dw",
+  authDomain: "tik-tok-clone-974d3.firebaseapp.com",
+  projectId: "tik-tok-clone-974d3",
+  storageBucket: "tik-tok-clone-974d3.appspot.com",
+  messagingSenderId: "171263556661",
+  appId: "1:171263556661:web:80c45f087ba9b88881057c",
+  measurementId: "G-911XRJQCW9"
+};
